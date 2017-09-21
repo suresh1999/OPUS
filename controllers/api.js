49377446a54d6ea1dacdee794aab837caf13f1d5
Suresh Kumar <Suresh Kumar>
2017-09-22 00:03:36 +0530

    added contoller:

--- controllers/api.js
+++ controllers/api.js
@@ -11,24 +11,42 @@ router.post('/login', function(req,res){
     var username = req.body.username;
     var password = req.body.password;
     console.log(username);
-    pool.query('SELECT * from user where username = ?',[username],function(err,result){
+    pool.query('SELECT * from user where username = ? and password = ?',[username,password],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } else {
-            res.json(result)
+            if(result.length){
+                res.send(200).json({"message":"login successfull"});
+            }
+            else{
+                res.json(500,{"error":"username/password is invalid"});
+            }
         }
     });
+})
+
+router.get('/username/:name',function(req,res){
+    var username = req.param.name;
+    pool.query('SELECT username from user where username = ?',[username],function(err,result){
+        if(err){
+            res.status(500).json({"error":true,"msg":err.toString()});
+        }
+        else if(result.length == 0){
+            res.status(200).json({"error":false,"msg":"this username is available"});
+        } else {
+            res.status(200).json({"error":true,"msg":"this username is available"});
+        }
+    })
 })
 
 router.post('/register',function(req,res){
     var username = req.body.username;
     var password = req.body.password;
-    console.log(req.body)
     pool.query('INSERT into user values(?,?)',[username,password],function(err,result){
         if(err){
             res.status(500).send(err.toString());
         } else {
-            res.send(result)
+            res.status(200).json({"success":true, "msg":"user account successfully created"});
         }
     });
 
